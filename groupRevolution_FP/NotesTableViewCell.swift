04f0b0620adb726925913c90539249dafc080e42
Syed Nooruddin Fahad <C0765774@mylambton.ca>
2020-06-23 17:06:15 -0400

    NotesTableViewCell Connections

--- groupRevolution_FP/NotesTableViewCell.swift
+++ groupRevolution_FP/NotesTableViewCell.swift
@@ -6,10 +6,16 @@
 //  Copyright © 2020 Naveen Kumar. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
 class NotesTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
