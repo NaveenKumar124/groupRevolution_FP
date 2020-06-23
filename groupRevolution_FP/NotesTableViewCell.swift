3270fb28807eaf359f5864128ce2adbd70e38b5a
Syed Nooruddin Fahad <C0765774@mylambton.ca>
2020-06-23 16:58:08 -0400

    NotesTableViewCell

---
+++ groupRevolution_FP/NotesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotesTableViewCell.swift
+//  groupRevolution_FP
+//
+//  Created by Syed Nooruddin Fahad on 23/06/20.
+//  Copyright © 2020 Naveen Kumar. All rights reserved.
+//
+
+import UIKit
+
+class NotesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
