8f39b4d95925b2dd83c404f5494d6ae21f6c859a
Syed Nooruddin Fahad <C0765774@mylambton.ca>
2020-06-24 13:18:33 -0400

    NotesTableViewController

--- groupRevolution_FP/NotesTableViewController.swift
+++ groupRevolution_FP/NotesTableViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class NotesTableViewController: UITableViewController {
     
     var folderName: String?
-    var note: [NSManagedObject]?
+    var notes: [NSManagedObject]?
     var context: NSManagedObjectContext?
 
     override func viewDidLoad() {
@@ -34,39 +34,66 @@ class NotesTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return note?.count ?? 0
+        return notes?.count ?? 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "noteCell") as! NotesTableViewCell
+        cell.setData(note: notes![indexPath.row])
         return cell
     }
-    */
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            self.context!.delete(self.notes![indexPath.row])
+            self.notes?.remove(at: indexPath.row)
+            saveData()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
+    
+    func saveData(){
+        do{
+            try context!.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func loadData() {
+        notes = []
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Notes")
+        request.predicate = NSPredicate(format: "category = %@", folderName!)
+        
+        do{
+            let results = try context!.fetch(request)
+            if results is [NSManagedObject]{
+                if results.count > 0{
+                    notes = results as! [NSManagedObject]
+                }
+            }
+            
+            tableView.reloadData()
+        }catch{
+            print(error)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadData()
+    }
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
@@ -81,15 +108,4 @@ class NotesTableViewController: UITableViewController {
         return true
     }
     */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
