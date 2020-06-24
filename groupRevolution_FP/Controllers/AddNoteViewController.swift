29ad717ffba6d6b84e1246735ac99745d215b976
Syed Nooruddin Fahad <C0765774@mylambton.ca>
2020-06-24 19:31:43 -0400

    AddNoteViewController Added

---
+++ groupRevolution_FP/Controllers/AddNoteViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddNoteViewController.swift
+//  groupRevolution_FP
+//
+//  Created by Syed Nooruddin Fahad on 24/06/20.
+//  Copyright © 2020 Naveen Kumar. All rights reserved.
+//
+
+import AVFoundation
+import CoreData
+import CoreLocation
+import UIKit
+
+class AddNoteViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate , AVAudioRecorderDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
