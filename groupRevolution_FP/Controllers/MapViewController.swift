09d3f07e39aa5d3186591677a1ec6039dc6f009f
navimalhotra <navimalhotra124@gmail.com>
2020-06-25 13:11:10 -0400

    map

--- groupRevolution_FP/Controllers/MapViewController.swift
+++ groupRevolution_FP/Controllers/MapViewController.swift
@@ -22,11 +22,64 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+         mapView.delegate = self
+               
+               locationManager.delegate = self
+               locationManager.desiredAccuracy = kCLLocationAccuracyBest
+               locationManager.startUpdatingLocation()
+               showSavedLocation()
     }
     
     
+    @IBAction func btnShowRoute(_ sender: UIButton) {
+        
+    }
     
+    @IBAction func navButtonPressed(_ sender: UIButton) {
+        
+        let dest = CLLocationCoordinate2D(latitude: segueLatitude, longitude: segueLongitude)
+               getDirection(dest: dest)
+        
+    }
+    
+    func showSavedLocation(){
+        
+        let span = MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0)
+        let location = CLLocationCoordinate2D(latitude: segueLatitude!, longitude: segueLongitude!)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = Annotations(coordinate: location, identifier: "pinAnnotation")
+        
+        //        annotation.coordinate = location
+        mapView.addAnnotation(annotation)
+    }
+    
+    
+    func getDirection(dest: CLLocationCoordinate2D){
+        
+        let source = MKMapItem(placemark:MKPlacemark(coordinate: mapView.userLocation.coordinate))
+        let dest = MKMapItem(placemark: MKPlacemark(coordinate: dest))
+        
+        MKMapItem.openMaps(with: [source , dest], launchOptions: [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving ])
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        if overlay is MKPolyline{
+            
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            
+            renderer.strokeColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+            renderer.lineWidth = 2
+            
+            return renderer
+            
+        }
+        return MKOverlayRenderer()
+    }
     
 
     /*
